7c496a31379283068a563f87f2e9741e3fbefcef
Mantas Staknys <mantas.staknys@stud.techin.lt>
2025-04-18 10:27:06 +0300

    about page header media queries and fixes

--- JS/about_script.js
+++ JS/about_script.js
@@ -1,5 +1,53 @@
 'use strict';
 
+// burger menu
+
+let burgerBtn = document.getElementById("mobile-btn");
+let header = document.getElementById("header")
+let main = document.getElementById("main")
+let footer = document.getElementById("footer")
+let brgMenuParent = document.body;
+let brgMenu = document.getElementById("brg-menu");
+let menuOn = false;
+
+
+header.style.transition = "0.2s ease-in-out"
+main.style.transition = "0.2s ease-in-out"
+footer.style.transition = "0.2s ease-in-out"
+
+document.addEventListener("click", e => {
+    if (e.target.id != "brg-menu" && menuOn && e.target.id != "mob") {
+        header.style.filter = "brightness(100%)"
+        main.style.filter = "brightness(100%)"
+        footer.style.filter = "brightness(100%)"
+        brgMenu.style.right = "-255px";
+        menuOn = false;
+    }
+})
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+        header.style.filter = "brightness(100%)"
+        main.style.filter = "brightness(100%)"
+        footer.style.filter = "brightness(100%)"
+        brgMenu.style.right = "-255px";
+        menuOn = false;
+    }
+})
+
+burgerBtn.addEventListener("click", function() {
+    
+    header.style.filter = "brightness(50%)"
+    main.style.filter = "brightness(50%)"
+    footer.style.filter = "brightness(50%)"
+    brgMenu.style.right = "0px";
+    menuOn = true;
+})
+
+
+// burger menu end
+
+
 let krestik = document.getElementsByClassName('circle_div_another');
 let circle = document.getElementsByClassName('circle_div');
 let maincontainer_all = document.getElementsByClassName("maincontainer_all");
