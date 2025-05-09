b39fdfe0b9f27f80cf04dd4c0a92b191a8e76470
Evanas Daujotas <evanas.daujotas@stud.techin.lt>
2025-05-09 11:55:36 +0300

    dsadsa

--- JS/contact.js
+++ JS/contact.js
@@ -1,6 +1,6 @@
-
 let form = document.forms[0];
 
+let inputdede = document.getElementsByClassName('mememe')
 let inputs = document.getElementsByClassName('formss')
 
 let correct = 0;
@@ -9,13 +9,17 @@ form.addEventListener('submit', e=>{
     e.preventDefault();
     correct = 0;
     console.log(inputs)
-
-    Array.from(inputs).forEach(e => {
+    
+    Array.from(inputs).forEach((e, er) => {
         if(e.value.trim().length == 0){
             e.style.borderColor = '#F67E7E';
+            e.style.color = '#F67E7E';
+            inputdede[er].style.display = 'block';
         }
         else{
             e.style.borderColor = '';
+            e.style.color = '';
+            inputdede[er].style.display = 'none';
         };
     });
     for(let i = 0; i < inputs.length; i++){
